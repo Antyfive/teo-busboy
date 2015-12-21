e24a97e691f30d8ffea8a6736f583bf9c955aadf
Andrew Teologov <teologov.and@gmail.com>
2015-12-21 21:29:18 +0200

    continue on scaffolding

--- index.js
+++ index.js
@@ -6,45 +6,6 @@
 
 "use strict";
 
-const
-    EventEmitter = require("events").EventEmitter,
-    Busboy = require("busboy"),
-    _ = require("lodash"),
-    dropStream = require("./lib/dropStream");
+const Multipart = require("./lib/multipart");
 
-module.exports = class TeoMultipart extends EventEmitter {
-    constructor(req, options) {
-
-        _.bindAll(this, ["onFile", "onField", "onFinish", "onError", "onClose"]);
-
-        this.busboy = new Busboy({headers: req.headers});
-        this.busboy
-            .on("file", this.onFile)
-            .on("field", this.onField)
-            .on("finish", this.onFinish)
-            .on("error", this.onError)
-            .on("close", this.onClose)
-            .("partsLimit", () => {
-                // 413
-            })
-            .("filesLimit", () => {
-                // 413
-            })
-            .("fieldsLimit", () => {
-                // 413
-            });
-    }
-
-    onFile(fieldname, file, filename, encoding, mimetype) {
-
-    }
-    onField(fieldname, val, fieldNameTruncated, valTruncated) {
-
-    }
-
-    onFinish() {}
-
-    onError() {}
-
-    onClose() {}
-};
+module.exports = Multipart;
