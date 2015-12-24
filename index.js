a241882c7674d96f63cdd56779fbdef92aa17879
Andrew Teologov <teologov.and@gmail.com>
2015-12-24 09:16:04 +0200

    minor

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /*!
- * Teo multipart
+ * Teo-busboy
  * @author Andrew Teologov <teologov.and@gmail.com>
  * @date 12/20/15
  */
 
 "use strict";
 
-const Multipart = require("./lib/teoBusboy");
+const TeoBusboy = require("./lib/teoBusboy");
 
-module.exports = Multipart;
+module.exports = TeoBusboy;
